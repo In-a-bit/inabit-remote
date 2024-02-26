70eb63b20ab91908e0f2dc6baf7b603cf4145a54
orib <orib@proftit.com>
2024-02-26 13:16:20 +0000

    fix issue of authorization token trimming whitespaces

--- apps/approver/src/utils/utils.service.ts
+++ apps/approver/src/utils/utils.service.ts
@@ -24,7 +24,7 @@ export class UtilsService {
     try {
       const config = {
         headers: {
-          Authorization: 'Bearer ' + authorizationToken,
+          Authorization: 'Bearer ' + authorizationToken.trim(),
           'Access-Control-Allow-Origin': '*',
         },
       };
